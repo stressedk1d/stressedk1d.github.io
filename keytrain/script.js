8c1cc8c24f20b09fa70f98215db0366fcf096f55
stressedk1d <gromov-igor2006.com@yandex.ru>
2024-10-14 23:28:45 +0300

    Init4

--- keytrain/script.js
+++ keytrain/script.js
@@ -1,41 +1,46 @@
-const listKeys = ['Q', 'W', 'E', 'D', 'F'];
+const listKeys = ['A', 'S', 'D', 'F', 'G'];
 let currentKey = '';
 let score = 0;
 let timer = 5;
-let intervalId; 
+let intervalId;
+let randnumber=0;
 
 
 const fallingKeyElement = document.getElementById('fallingKey');
 const scoreElement = document.getElementById('score');
 const timerElement = document.getElementById('timer');
-const outputElement = document.getElementById('output');
+const numberElement = document.getElementById('number');
+const unusedKeysElement = document.getElementById('unused');
 
-
+function updateUnusedKeys() {
+    unusedKeysElement.textContent = 'Неиспользованные клавиши: ' + listKeys.join(', ');
+}
 
 function startGame() {
     if (listKeys.length === 0) {
-        outputElement.textContent='Игра окончена! Ваши очки: ' + score;
+        alert('Игра окончена! Ваши очки: ' + score);
+        listKeys.push('A', 'S', 'D', 'F', 'G');
+        score = 0;
         return;
     }
    
-    let nkey=Math.floor(Math.random() * listKeys.length);
-    currentKey = listKeys[nkey];
-    console.log[nkey];
+
+    currentKey = listKeys[Math.floor(Math.random() * listKeys.length)];
+    randnumber=listKeys.indexOf(currentKey);
+    numberElement.textContent =randnumber;
     fallingKeyElement.textContent = currentKey;
 
-
     fallingKeyElement.style.top = '0px';
-    fallingKeyElement.style.left = Math.random() * (300 - 50) + 'px'; // Случайная горизонтальная позиция
-
+    fallingKeyElement.style.left = Math.random() * (300 - 50) + 'px';
 
     timer = 5;
     timerElement.textContent = timer;
 
+    updateUnusedKeys();
 
     intervalId = setInterval(() => {
         timer--;
         timerElement.textContent = timer;
-
 
         if (timer <= 0) {
             alert('Время вышло! Игра окончена.');
@@ -58,18 +63,11 @@ function startGame() {
 
 document.addEventListener('keydown', (event) => {
     if (event.key.toUpperCase() === currentKey) {
-        score += timer; 
-        scoreElement.textContent = score; 
+        score += timer;
+        scoreElement.textContent = score;
 
-
-        
         listKeys.splice(listKeys.indexOf(currentKey), 1);
-       
         clearInterval(intervalId);
         startGame();
     }
 });
-
-function Start() {
-    startGame();
-}
