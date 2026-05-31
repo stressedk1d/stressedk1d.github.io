6cb81100cfdcc616d0ce49f1c2dc3861e9c0afe9
stressedk1d <181460099+stressedk1d@users.noreply.github.com>
2026-05-31 19:22:06 +0300

    Remove custom domain, deploy site to GitHub Pages

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SITE_CONFIG = {
   email: "gromov-igor2006.com@yandex.ru",
   goatCounter: "",
-  customDomain: "0bsession.dev",
+  customDomain: "",
   projects: {
     marketplace: {
       demoUrl: "https://vogueway.ru/",
